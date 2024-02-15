992d5da04e1939b677cfe39be34101896538dc08
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-15 08:37:44 +0300

    Include greetings routes in routes file Edit database.yml to extract values from env

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "root#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
 end

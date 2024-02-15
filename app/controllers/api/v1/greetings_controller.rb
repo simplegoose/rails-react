7f9d9b6742b8c7069d5b2389bdc31bda8af43a85
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-02-15 08:36:59 +0300

    Create a controller for greetings api

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+
+      # Get api/v1/greetings
+      def index
+        @greetings = Greeting.all
+        @greetingsCount = Greeting.count
+
+        random_greeting_id = rand(@greetingsCount)
+
+        render json: @greetings[random_greeting_id], status: :ok
+      end
+    end
+  end
+end
